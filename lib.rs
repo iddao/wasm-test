9a277fcf32b676eb06dbe18cb5ce7b4e98d571f1
nope0124 <kami-ryo0124@ezweb.ne.jp>
2022-05-03 01:26:56 +0900

    :tada: Initial Commit

---
+++ lib.rs
@@ -0,0 +1,69 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+
+
+#[ink::contract]
+mod flipper {
+    use hex_literal::hex;
+    extern crate hex_slice;
+    
+
+   
+    #[ink(storage)]
+    pub struct Flipper {
+        value: bool,
+        // AUTH_CERT: struct TheTupleStruct(&[u8]),
+    }
+    // named_tuple!(
+    //     #[derive(SpreadLayout)]
+    //     pub struct Flipper {
+    //         value: bool,
+    //         AUTH_CERT: &'static [u8],
+    //     }
+    // );
+
+    impl Flipper {
+        #[ink(constructor)]
+        pub fn new(init_value: bool) -> Self {
+            Self {
+                value: init_value,
+                
+            }
+        }
+
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            self.value = !self.value;
+        }
+
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn default_works() {
+            let flipper = Flipper::default();
+        }
+
+        #[ink::test]
+        fn it_works() {
+            let mut flipper = Flipper::new(false);
+            flipper.flip();
+        }
+    }
+}
